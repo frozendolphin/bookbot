265989903199bfba371b739a561344dbec279429
frozendolphin <frozendolphin27@gmail.com>
2024-12-12 09:48:22 +0000

    wrapped open and read file feature in main()

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-with open("./books/frankenstein.txt") as f:
-    file_contents = f.read() 
+def main():
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()  
+        print(file_contents)
+        
+if __name__ == "__main__":
+    main()
