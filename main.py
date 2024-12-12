8bd5b95e15537f6b680e951f93c0c71797b449b2
frozendolphin <frozendolphin27@gmail.com>
2024-12-12 09:57:41 +0000

    added feature to count the number of words

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()  
-        print(file_contents)
-        
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    total_words = count_words(text)
+    print(total_words)
+    
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(complete_text):
+    words = complete_text.split()
+    return len(words)
+    
 if __name__ == "__main__":
     main()
